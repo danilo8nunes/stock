0ef0edf3f36accb8016179fce43fa61c98e2b872
Yago Henrique <yagodevweb@gmail.com>
2020-05-30 17:27:41 -0300

    refatorando o código

--- class/entries.php
+++ class/entries.php
@@ -1,8 +1,8 @@
 <?php
 require "products.php";
 
-class Entries extends Connection{
-
+class Entries extends Connection 
+{
 	public function addEntries($id_prod, $price, $quantity, $date)
 	{
 		$query = "INSERT INTO entries (id_prod, purchase_price, quantity, date) VALUES (:id_prod, :price, :quantity, :date)";
@@ -13,28 +13,26 @@ class Entries extends Connection{
 		$sql->bindValue(':date', $date);
 		$sql->execute();
 
-		if ($sql->errorCode() != "00000") {
+		if ($sql->errorCode() == "00000") {
 			return false;
-		 
-		} else {
-			$inject = true;
-			$addQuatity = $this->updateQuantity($id_prod, $quantity, $inject);
-
-			if ($addQuatity == true){
-				return true;
+		}
 			
-			} else {
-				return false;
-			}
+		$addQuatity = $this->updateQuantity($id_prod, $quantity, true);
+
+		if ($addQuatity == true){
+			return true;
+		} else {
+			return false;
 		}
 	}
-	public function getEntries($search = "")
+
+	public function getEntries($search = null)
 	{
-		if ($search == ""){
-			$query = "SELECT entries.id, entries.id_prod, entries.purchase_price, entries.quantity, entries.date, products.name FROM entries INNER JOIN products ON entries.id_prod = products.id ORDER BY date DESC";
-		
+		if ($search) {
+			$query = "SELECT E.id, E.id_prod, E.purchase_price, E.quantity, E.date, P.name FROM entries AS E INNER JOIN products AS P ON E.id_prod = P.id WHERE name LIKE :search ORDER BY date DESC";
+
 		} else {
-			$query = "SELECT entries.id, entries.id_prod, entries.purchase_price, entries.quantity, entries.date, products.name FROM entries INNER JOIN products ON entries.id_prod = products.id WHERE name LIKE :search ORDER BY date DESC";
+			$query = "SELECT entries.id, entries.id_prod, entries.purchase_price, entries.quantity, entries.date, products.name FROM entries INNER JOIN products ON entries.id_prod = products.id ORDER BY date DESC";
 		}
 
 		$sql = $this->pdo->prepare($query);
@@ -53,6 +51,7 @@ class Entries extends Connection{
 			}
 		}
 	}
+
 	public function delEntries($id, $id_prod, $quantity)
 	{
 		$query = "DELETE FROM entries WHERE id = :id";
@@ -63,26 +62,34 @@ class Entries extends Connection{
 		if ($sql->errorCode() != "00000") {
 			return false;
 		 
-		} else {
-			$inject = false;
-			$update = $this->updateQuantity($id_prod, $quantity, $inject);
-			
-			if ($update == false){
-				return false;
-			
-			} else {
-				return true;
-			}
 		}
+
+		$update = $this->updateQuantity($id_prod, $quantity, false);
+		
+		if ($update == false) {
+			return false;
+		} 
+
+		return true;
 	}
-	private function updateQuantity($id_prod, $quantity, $inject)
+
+		
+	/**
+	 * Altera a quantidade de produtos
+	 *
+	 * @param  int $id_prod
+	 * @param  int $quantity
+	 * @param  bool $inject (true = incremento, false = decremento)
+	 * @return bool
+	 */
+	private function updateQuantity($id_prod, $quantity, $inject): bool
 	{
-		if ($inject == true){
-			$query = "UPDATE products SET quantity = quantity + :quantity WHERE id = :id_prod;";	
+		if ($inject) {
+			$query = "UPDATE products SET quantity = quantity + :quantity WHERE id = :id_prod";	
+		} else {
+			$query = "UPDATE products SET quantity = quantity - :quantity WHERE id = :id_prod";	
 		}
-		else {
-			$query = "UPDATE products SET quantity = quantity - :quantity WHERE id = :id_prod;";	
-		}
+
 		$sql = $this->pdo->prepare($query);
 		$sql->bindValue(':quantity', $quantity);
 		$sql->bindValue(':id_prod', $id_prod);
@@ -90,11 +97,9 @@ class Entries extends Connection{
 
 		if ($sql->errorCode() != "00000") {
 			return false;
-		 
-		} else {
-			return true;	
+		}
 
-		}	
+		return true;
 	}
 	
 }
