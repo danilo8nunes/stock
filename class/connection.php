0eabd92c5941d2d9f87b8d0fdc20a74b38f2d3c8
Yago Henrique <yagodevweb@gmail.com>
2020-05-30 17:27:13 -0300

    trocando modificador para protected

--- class/connection.php
+++ class/connection.php
@@ -1,16 +1,16 @@
 <?php
-class Connection{
 
-	public $pdo;
+class Connection
+{
+	protected $pdo;
 
 	public function __construct() 
 	{
 		try {
 			$this->pdo = new PDO("mysql:dbname=stock;host=localhost", "root", "");
-		
-		} catch(PDOException $e){
-			echo "Erro".$e->getMessage();
+
+		} catch(PDOException $e) {
+			echo "Erro: " . $e->getMessage();
 		}
 	}
 }
-?>
